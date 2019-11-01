ccd347d52a29d9216bb02eec3eaeff4c3ce2013e
Guillermo Parral <guillermoeparral@gmail.com>
2019-10-31 22:36:48 -0300

    finished server

--- server/index.js
+++ server/index.js
@@ -1,52 +1,17 @@
 const express = require('express');
 const cors = require('cors');
-const https = require('https');
+const service = require('./service');
 
 const app = express();
 
 app.use(cors());
 
 app.get('/api/items', (req, res) => {
-    let body = '';
-    https.get(`https://api.mercadolibre.com/sites/MLA/search?q=${req.query.q}`, (response) => {
-        response.on('data', (chunk) => {
-            body += chunk;
-        })
-        response.on('end', () => {
-            const json = JSON.parse(body);
-            res.json(json);
-        })
-    }).on('error', (error) => console.error(error));
-})
+    service.getItemsList(req.query.q).then(items => res.json(items));
+});
 
 app.get('/api/items/:id', (req, res) => {
-    const promisifiedItem = new Promise((resolve, reject) => {
-        let body = '';
-        https.get(`https://api.mercadolibre.com/items/${req.params.id}`, (response) => {
-            response.on('data', (chunk) => {
-                body += chunk;
-            })
-            response.on('end', () => {
-                resolve(JSON.parse(body));
-            })
-        }).on('error', (error) => reject(error));
-    });
-    const promisifiedDescription = new Promise((resolve, reject) => {
-        let body = '';
-        https.get(`https://api.mercadolibre.com/items/${req.params.id}/description`, (response) => {
-            response.on('data', (chunk) => {
-                body += chunk;
-            })
-            response.on('end', () => {
-                resolve(JSON.parse(body));
-            })
-        }).on('error', (error) => reject(error));
-    });
-    Promise.all([promisifiedItem, promisifiedDescription]).then((responses) => {
-        const item = responses[0];
-        const desc = responses[1];
-        res.json({item: item, description: desc});
-    }).catch(error => console.log(error));
+    service.getItemDetail(req.params.id).then(item => res.json(item));
 });
 
 app.listen(8080, () => {
---
+++ server/service.js
@@ -0,0 +1,59 @@
+const client = require('./client');
+
+exports.getItemsList = (query) => {
+    return client.search(query).then((response => {
+        const author = {
+            name: 'Guillermo',
+            lastname: 'Parral'
+        };
+        const categories = response.filters.values ?
+            response.filters.values[0].path_from_root.map((elem) => elem.name) : [];
+        const items = response.results.map((item) => {
+            const [amount, decimals] = item.price.toString().split('.');
+            return {
+                id: item.id,
+                title: item.title,
+                price: {
+                    currency: item.currency_id,
+                    amount: parseInt(amount),
+                    decimals: parseInt(decimals)
+                },
+                picture: item.thumbnail,
+                condition: item.condition,
+                free_shipping: item.shipping.free_shipping
+            }
+        });
+        return {
+            author,
+            categories,
+            items
+        }
+    }));
+};
+
+exports.getItemDetail = (query) => {
+    return client.getItemData(query).then(responses => {
+        const [item, description] = responses;
+        const [amount, decimals] = item.price.toString().split('.');
+        return {
+            author: {
+                name: 'Guillermo',
+                lastname: 'Parral'
+            },
+            item: {
+                id: item.id,
+                title: item.title,
+                price: {
+                    currency: item.currency_id,
+                    amount: parseInt(amount),
+                    decimals: parseInt(decimals)
+                },
+                picture: item.thumbnail,
+                condition: item.condition,
+                free_shipping: item.shipping.free_shipping,
+                sold_quantity: item.sold_quantity,
+                description: description.plain_text
+            }
+        }
+    })
+};

6fdd4062d24fea635dcd9ae2b2528b06c5217765
Guillermo Parral <guillermoeparral@gmail.com>
2019-11-03 20:23:20 -0300

    only show 4 items. some style fixes

--- src/ItemsList/ItemsList.js
+++ src/ItemsList/ItemsList.js
@@ -6,6 +6,6 @@ import Breadcrumb from "../Breadcrumb/Breadcrumb";
 export default function ItemsList(props){
     return <div className={"items-list-container"}>
         <Breadcrumb categories={props.categories}/>
-        {props.items.map((item, idx) => <Item key={idx} info={item} categories={props.categories}/>)}
+        {props.items.slice(0,4).map((item, idx) => <Item key={idx} info={item} categories={props.categories}/>)}
     </div>
 }
